96097e879ef6916a46a0d37e36c1d9506bb5af7b
Rahul Kumar <99110166+CrisPBacon84@users.noreply.github.com>
2023-02-13 21:58:29 +0530

    Add files via upload

---
+++ data types.js
@@ -0,0 +1,17 @@
+// VARIABLEs 
+var name = 'hello world';
+console.log(name);
+let name = 'rahul';
+console.log(typeof name);
+
+let age = 66;
+console.log(typeof age);
+
+let isMarried = false;
+console.log(typeof isMarried);
+
+let colors;
+console.log(typeof colors);
+
+let selectColor = 'null';
+console.log(typeof selectColor);
