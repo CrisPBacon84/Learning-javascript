1435107fa7fb8e1f304605cb11fab1e0c30f8ed6
Rahul Kumar <99110166+CrisPBacon84@users.noreply.github.com>
2023-02-15 16:50:25 +0530

    Add files via upload

---
+++ Reference types.js
@@ -0,0 +1,19 @@
+// reference types
+
+let name = 'rahul';
+let age = '18';
+let hobby = 'chess';
+
+let user ={
+    name : 'rahul',
+    age : '18',
+    hobby : 'chess',
+};
+
+console.log(user);
+console.log(user.name);
+console.log(user.age);
+console.log(user.hobby)
+
+console.log(user['name']);
+console.log(user['age']);
