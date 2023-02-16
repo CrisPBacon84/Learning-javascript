21e851f22809e2541ae4efdfab019756b1fe309c
Rahul Kumar <99110166+CrisPBacon84@users.noreply.github.com>
2023-02-16 21:26:42 +0530

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,45 @@
+//arithmetic operators
+
+let num1 = 10;
+let num2 = 20;
+
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1*num2);
+console.log(num1/num2);
+console.log(num1**num2);
+console.log(num1%num2);
+
+//assignment operators
+/*
+let num1 = 10;
+let num2 = num1 + 5;
+num1 +=5;
+console.log(num2);
+
+num1 = num1 - 5;
+num1 -= 5;
+num1 =num*5;
+num1 *= 5;
+num1 = num1 +1;
+num1 +=1;
+num1++;*/
+
+//comparison operators
+
+let price= 200;
+console.log(price < 300);
+console.log(price <= 300);
+console.log(price >= 300);
+console.log(price === 300);
+console.log(price !== 300);
+
+//logical operators
+// and(&&) , or(||) , not(!)
+
+let gas = 20;
+console.log(gas<50 && gas > 10);
+console.log(gas< 4 || gas > 5);
+console.log(!(gas>100));
+
